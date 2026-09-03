f2e80837a8139d5058821a4cad5c7e6713ba4e20
AshleyCol <ashe.coleman1022@gmail.com>
2026-09-02 23:37:23 -0400

    completed round calculations, score counter, and final results

--- RockPaperScissors Project #1/RockPaperScissors Project #1.cpp
+++ RockPaperScissors Project #1/RockPaperScissors Project #1.cpp
@@ -16,29 +16,47 @@ int main()
     int ties = 0;
     int rounds = 0;
     int currentHand = 0;
+    int result = 0;
     bool playing = true;
 
     while (playing) {
+
         displayMenu();
 
         currentHand = getInput();
+
         if (currentHand != 4) {
-            getRoundWinner(currentHand);
+
+            result = getRoundWinner(currentHand);
+            if (result == 1) {
+                ties += 1;
+                cout << "You tied!\n";
+            }
+            else if (result == 2) {
+                wins += 1;
+                cout << "You won!\n";
+            }
+            else {
+                losses += 1;
+                cout << "They won!\n";
+            }
+
+            rounds += 1;
         }
         else {
-            displayFinalResult(wins, losses, ties, rounds);
-            return 0;
+            playing = false;
         }
         
-        rounds += 1;
     }
             
 
-
+    displayFinalResult(wins, losses, ties, rounds);
+    return 0;
 }
 
 void displayMenu() {
     cout << "Please pick a hand to play below or select 4 to quit: \n" << "    1. Rock\n" << "    2. Paper\n" << "    3. Scissors\n" << "    4. End Game & show score\n";
+    return;
 }
 
 int getInput() {
@@ -58,10 +76,70 @@ int getInput() {
     return option;
 }
 
-int getRoundWinner(int) {
+int getRoundWinner(int hand) {
+    random_device engine;
+    uniform_int_distribution<int> randomness(1, 3);
+    int computerHand = randomness(engine);
 
+
+    if (hand == 1) {
+        cout << "You played rock\n";
+    }
+    else if (hand == 2) {
+        cout << "You played paper\n";
+    }
+    else if (hand == 3) {
+        cout << "You played scissors\n";
+    }
+
+    if (computerHand == 1) {
+        cout << "They played rock\n";
+    }
+    else if (computerHand == 2) {
+        cout << "They played paper\n";
+    }
+    else if (computerHand == 3) {
+        cout << "They played scissors\n";
+    }
+
+
+    if (hand == computerHand) {
+        return 1;
+    }
+    else if (hand == 1 && computerHand == 3) {
+        return 2;
+    }
+    else if (hand == 2 && computerHand == 1) {
+        return 2;
+    }
+    else if (hand == 3 && computerHand == 2) {
+        return 2;
+    }
+    else if (hand == 1 && computerHand == 2) {
+        return 3;
+    }
+    else if (hand == 2 && computerHand == 3) {
+        return 3;
+    }
+    else if (hand == 3 && computerHand == 1) {
+        return 3;
+    }
 }
 
 void displayFinalResult(int wins, int losses, int ties, int roundsPlayed) {
+    cout << "You've ended the game after " << roundsPlayed << " rounds!\n";
+    cout << "You won " << wins << " times, the computer won " << losses << " times, and you tied " << ties << " times!\n";
 
+    if (wins == losses) {
+        cout << "In the end, you tied!";
+        return;
+    }
+    else if (wins - losses > 0) {
+        cout << "In the end, you won! Congratulations!";
+        return;
+    }
+    else if (wins - losses < 0) {
+        cout << "In the end, the computer won. Better luck next time!";
+        return;
+    }
 }
